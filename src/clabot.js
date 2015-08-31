15ff66365ac25c97b83ca3927daffda89b5be4a5
Robbie Mackay <rm@robbiemackay.com>
2015-08-31 14:09:41 +1200

    Add form and storage based on clabot-sample

--- src/clabot.js
+++ src/clabot.js
@@ -1,16 +1,53 @@
+var express = require('express');
+var jade = require('jade');
+var data = require('./lib/data');
+var validate = require('./lib/validate');
+
+// Setup clabot
 var app = require('clabot').createApp({
-  getContractors: function(callback){ callback(['list','of','contributors'])},
+  getContractors: data.getContractors,
   token: process.env.GITHUB_TOKEN,
   templateData: {
     link: '$http://your-cla-webform.com',
-    maintainer: '$githubusername'
+    maintainer: process.env.MAINTAINER
   },
   secrets: {
-    '$repoowner': {
-      '$sandbox': process.env.HUB_SECRET
+    'ushahidi': {
+      'platform': process.env.HUB_SECRET,
+      'platform-client': process.env.HUB_SECRET,
     }
-  }
+  },
+  skipCollaborators: true,
+  skipContributors: true
 });
+
+// Serve assets
+app.use(require('connect-assets')());
+
+app.use(express.compress());
+
+app.get('/form/:project/:kind?', function(req, res) {
+  var kind, project;
+  project = req.params.project;
+  // Makes no sense, yet. Extensible in the future.
+  if (project !== 'platform') { project = 'platform'; }
+
+  // Format kind as Uppercasefirstchar
+  kind = req.params.kind;
+  kind = (kind != null) ? kind.charAt(0).toUpperCase() + kind.slice(1).toLowerCase() : '';
+  if (kind !== 'Entity') { kind = 'Individual'; }
+
+  // Render the form
+  return res.render('form.jade', {
+    agreement: "" + project + " " + kind + " Contributors License Agreement",
+    kind: kind,
+    layout: false,
+    project: project,
+    url: req.clabotOptions.templateData.link
+  });
+});
+
+app.post('/form', validate, data.save);
 
 port = process.env.PORT || 1337;
 
